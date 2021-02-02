6de59f29d0f31c7be099243a95c6929808ff07d2
Roy Mosby <ego.madking@gmail.com>
2021-02-02 10:49:53 -0500

    commit tree f22b6e2d0e7c1df686a15d57d5ae5c5a7e52d842

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,47 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
   # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end 
+
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :cook_time => params[:cook_time], :ingredients => params[:ingredients])
+    redirect to "/recipes/#{@recipe[:id]}"
+  end
+
+  get '/recipes/:id/edit' do
+    current_recipe(params[:id])
+    erb :edit
+  end
+
+  get "/recipes/:id" do
+    current_recipe(params[:id])
+    erb :recipe
+  end
+
+  patch "/recipes/:id" do
+    current_recipe(params[:id])
+    @recipe.update(:name => params[:name], :cook_time => params[:cook_time], :ingredients => params[:ingredients])
+    erb :recipe
+  end
+
+  delete "/recipes/:id" do
+    current_recipe(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
+
+  helpers do
+    def current_recipe(id)
+      @recipe = Recipe.find(id)
+    end
+  end
 
 end
